3c27083b4338a2b7fe10938c8db10d6aa7a3662a
phantomhdd <phantom.hdd@gmail.com>
2021-02-17 09:34:27 +0700

    fix trait

--- app/Traits/Uuid.php
+++ app/Traits/Uuid.php
@@ -11,6 +11,7 @@ trait Uuid {
      * @return void
      */
     protected static function boot() {
+        parent::boot();
         static::creating(function ($model) {
             if ( ! $model->getKey()) {
                 $model->{$model->getKeyName()} = (string) Str::uuid();
