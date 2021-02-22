9bf0ddad71e8c4f97c15b80ef8128a395f21946b
phantomhdd <phantom.hdd@gmail.com>
2021-02-22 23:17:32 +0700

    add queue listener

--- app/Listeners/SendEmailNotification.php
+++ app/Listeners/SendEmailNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class SendEmailNotification
+class SendEmailNotification implements ShouldQueue
 {
     /**
      * Create the event listener.
